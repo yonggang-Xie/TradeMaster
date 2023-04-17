f95bc45889f5ffaff8a25a53611e576b49aaa23b
yonggang-Xie <74223059+yonggang-Xie@users.noreply.github.com>
2023-04-17 12:28:10 +0800

    Update stock_collector.py

--- agent/aaai2020/tianshou/data/stock_collector.py
+++ agent/aaai2020/tianshou/data/stock_collector.py
@@ -316,8 +316,8 @@ class StockCollector_TwoActions(object):
             return np.array([data])
 
     def collect(self, n_step=0, n_episode=0, render=0):
-        print(n_step)
-        print(n_episode)
+        # print(n_step)
+        # print(n_episode)
         warning_count = 0
         if not self._multi_env:
             n_episode = np.sum(n_episode)
@@ -410,8 +410,8 @@ class StockCollector_TwoActions(object):
                 if sum(self._done):
                     obs_next = self.env.reset(np.where(self._done)[0])
                 if n_episode != 0:
-                    print(np.sum(np.array(cur_episode)))
-                    print(np.sum(np.array(n_episode))*0.9)
+                    # print(np.sum(np.array(cur_episode)))
+                    # print(np.sum(np.array(n_episode))*0.9)
                     if isinstance(n_episode, list) and \
                             np.sum(np.array(cur_episode)) >= np.sum(np.array(n_episode))*0.9 or \
                             np.isscalar(n_episode) and \
@@ -432,7 +432,7 @@ class StockCollector_TwoActions(object):
                     obs_next = self.env.reset()
                 if n_episode != 0 and cur_episode >= n_episode:
                     break
-            print(cur_step)
+            # print(cur_step)
             # print(warning_count)
             if n_step != 0 and cur_step >= n_step:
                 break
